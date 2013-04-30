73f11419983d1e997bf0a7ed50bd1a5d950ae09c
Jacob Kingery <jacob.kingery@students.olin.edu>
2013-04-30 19:52:44 -0400

    cleaned up formatting and added ability to see all three methods side-by-side

--- LiveWebCam.py
+++ LiveWebCam.py
@@ -1,71 +1,158 @@
 from __future__ import division
 from cv import *
-from time import time
 import os
 
-def run():
-	wc = CaptureFromCAM(0)	#selects the first camera found
-	asciiGroup = "#*oahbpqwmzcunxrt-+~<>:,^`. "		#ASCII characters used to represent image
-	windowsize = GetSize(QueryFrame(wc))	#Finding image size
-	hght = windowsize[1]
-	wdth = windowsize[0]
-	flipMode = 1 	#Mirror web cam image
-	fm = flipMode
-	threshold = 100		#Threshold for Threshold()
-	th = threshold
-	color = 255		#Color for Threshold()
-	clr = color
-	laplaceDegree = 3		#Degree of Lapaciation
-	ldeg = laplaceDegree
-	desiredwidth = 225		#Desired width of output
-	dw = desiredwidth
-	scaledsize = (dw, int(dw * .5 * hght // wdth))		#Scaled size based on desired width
 
-	frame = CreateImage(windowsize, 8, 1)		#Creating frame image
-	edges = CreateImage(windowsize, IPL_DEPTH_16S, 1)		#Creating edges image
-	scaled = CreateImage(scaledsize, 8, 1)		#Creating scaled image
+def capture_frame():
+    """Capture frame from webcam, convert to grayscale, flip, and smooth."""
+    CvtColor(QueryFrame(wc), inputframe, CV_RGB2GRAY)
+    Flip(inputframe, flipMode=fm)
+    Smooth(inputframe, inputframe, CV_MEDIAN)
 
-	while True:
-		CvtColor(QueryFrame(wc),frame,CV_RGB2GRAY)		#Capturing a frame from the web cam and converting it to grayscale
-		Flip(frame,flipMode=fm)		#Flipping image so that it is mirrored
-		Smooth(frame,frame,CV_MEDIAN)		#Smoothing image to reduce noise
-
-		'''Comment lines 34-40 and 50-57 and uncomment lines 60-65 to go back to just gradient/lapacian without the overlay'''
-		Resize(frame,scaled) 		#Resizing image to desired size
-		printed=''
-		for y in range(scaled.height):
-			printed = printed + ' '*int((304-dw)/2)		#Centering output in terminal window of width 304
-			for x in range(scaled.width):
-				printed = printed + asciiGroup[int(len(asciiGroup) - (scaled[y,x] * len(asciiGroup))//256 - 1)]		#Adding next character
-			printed = printed + '\n'*(y!=scaled.height-1)		#Adding newline for each row
+    return inputframe
 
 
-		Threshold(frame,frame,th,clr,CV_THRESH_BINARY)		#Creating black and white image
-		Laplace(frame,edges,ldeg)		#Doing Laplacian on image
-		Convert(edges,frame)		
+def hybrid_process(hybframe):
+    """Process frame using a gradient-Laplacian hybrid."""
+    hybprinted = gradient_process(hybframe)
 
-		Resize(frame,scaled)		#Resizing image to desired size
+    Threshold(hybframe, hybframe, th, clr, CV_THRESH_BINARY)
+    Laplace(hybframe, edges, ldeg)
+    Convert(edges, hybframe)
+    Resize(hybframe, scaled)
 
-		pixelnum = 0
-		for y in range(scaled.height):
-			pixelnum += int((304-dw)/2)
-			for x in range(scaled.width):
-				pixelnum += 1
-				if scaled[y,x] != 0:
-					printed = printed[:pixelnum-1] + asciiGroup[0] + (printed[pixelnum:]*(pixelnum!=len(printed)-1))		#Replacing character from gradient with # if it is present in edge-detected image
-			pixelnum += 1*(y!=(scaled.height-1))
+    pixelnum = 0
+    for y in range(scaled.height):
+        pixelnum += int((terminalwidth - dw) / 2)
+        for x in range(scaled.width):
+            pixelnum += 1
+            if scaled[y, x] != 0:
+                hybprinted = (hybprinted[:pixelnum - 1] + asciiGroup[0] + (
+                    hybprinted[pixelnum:] * (pixelnum != len(hybprinted) - 1)))
+        pixelnum += 1 * (y != (scaled.height - 1))
+
+    return hybprinted
 
 
-		# printed=''
-		# for y in range(scaled.height):
-		# 	printed = printed + ' '*int((304-dw)/2)
-		# 	for x in range(scaled.width):
-		# 		printed = printed + asciiGroup[int(len(asciiGroup) - (scaled[y,x] * len(asciiGroup))//256 - 1)]
-		# 	printed = printed + '\n'*(y!=scaled.height-1)
+def gradient_process(gradframe):
+    """Process frame using only a gradient."""
+    Resize(gradframe, scaled)
 
-		os.system('clear')		#Clearing terminal
-		print(printed)		#Printing string of ASCII characters
-		WaitKey(1)
+    printed = ''
+    for y in range(scaled.height):
+        printed = printed + ' ' * int((terminalwidth - dw) / 2)
+        for x in range(scaled.width):
+            printed = printed + asciiGroup[int(-1 + len(
+                asciiGroup) - ((scaled[y, x] * len(asciiGroup)) // 256))]
+        printed = printed + '\n' * (y != scaled.height - 1)
 
-if __name__=='__main__':
-	run()
+    return printed
+
+
+def laplacian_process(lapframe):
+    """Process frame using only a Laplacian."""
+    Threshold(lapframe, lapframe, th, clr, CV_THRESH_BINARY)
+    Laplace(lapframe, edges, ldeg)
+    Convert(edges, lapframe)
+    Resize(lapframe, scaled)
+
+    printed = ''
+    for y in range(scaled.height):
+        printed = printed + ' ' * int((terminalwidth - dw) / 2)
+        for x in range(scaled.width):
+            printed = printed + asciiGroup[int(-1 + len(
+                asciiGroup) - ((scaled[y, x] * len(asciiGroup)) // 256))]
+        printed = printed + '\n' * (y != scaled.height - 1)
+
+    return printed
+
+
+def all_process(rawframe):
+    """Process frame all three ways."""
+    Copy(rawframe, gradframe)
+    Copy(rawframe, lapframe)
+    Copy(rawframe, hybframe)
+    gradientprinted = gradient_process(gradframe).split('\n')
+    laplacianprinted = laplacian_process(lapframe).split('\n')
+    hybridprinted = hybrid_process(hybframe).split('\n')
+
+    printed = ''
+    for y in range(len(gradientprinted)):
+        gradienttrimmed = gradientprinted[y][(int((
+            terminalwidth - dw) / 2) - int((quadwidth - dqw) / 2)):]
+        printed = printed + gradienttrimmed + laplacianprinted[y] + '\n'
+    for y in range(len(hybridprinted)):
+        printed = printed + ' ' * (int((
+            terminalwidth - dqw) / 2) - int((terminalwidth - dw) / 2))
+        printed = printed + hybridprinted[
+            y] + '\n' * (y != (len(hybridprinted) - 1))
+
+    return printed
+
+
+def run(gradient, laplacian):
+    """Capture and process feed from webcam"""
+    while True:
+        rawframe = capture_frame()
+
+        if display == 'fullscreen':
+            if gradient and laplacian:
+                printed = hybrid_process(rawframe)
+            elif gradient:
+                printed = gradient_process(rawframe)
+            elif laplacian:
+                printed = laplacian_process(rawframe)
+            else:
+                return
+        elif display == 'quadrants':
+            printed = all_process(rawframe)
+
+        os.system('clear')
+        print(printed)
+        WaitKey(1)
+
+
+if __name__ == '__main__':
+    # Settings for frame capture
+    wc = CaptureFromCAM(0)
+    asciiGroup = "#*oahbpqwmzcunxrt-+~<>:,^`. "
+    windowsize = GetSize(QueryFrame(wc))
+    hght = windowsize[1]
+    wdth = windowsize[0]
+    flipMode = 1
+    fm = flipMode
+
+    # Settings for Threshold()
+    threshold = 100
+    th = threshold
+    color = 255
+    clr = color
+
+    # Setting for Laplace()
+    laplaceDegree = 3
+    ldeg = laplaceDegree
+
+    # Settings for sizing
+    display = 'fullscreen'
+    display = 'quadrants'
+    quadwidth = 152
+    desiredquadwidth = 110
+    dqw = desiredquadwidth
+    terminalwidth = 304
+    desiredwidth = 222
+    dw = desiredwidth
+    if display == 'fullscreen':
+        scaledsize = (dw, int(dw / 2 * hght // wdth))
+    elif display == 'quadrants':
+        scaledsize = (dqw, int(dqw / 2 * hght // wdth))
+
+    # Initialize OpenCV images
+    inputframe = CreateImage(windowsize, 8, 1)
+    rawframe = CreateImage(windowsize, 8, 1)
+    gradframe = CreateImage(windowsize, 8, 1)
+    lapframe = CreateImage(windowsize, 8, 1)
+    hybframe = CreateImage(windowsize, 8, 1)
+    edges = CreateImage(windowsize, IPL_DEPTH_16S, 1)
+    scaled = CreateImage(scaledsize, 8, 1)
+
+    run(gradient=True, laplacian=True)
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-This is a Software Design class project.  
-In order to view the ASCII-fied images, I recommend using Konsole instead of Terminal because of the faster printing speed.  
-The default size requires you to zoom out as far as possible (ctrl+- a few times).
+This is a Software Design class project.
+
+In order to view the ASCII-fied images, 
+I recommend using Konsole instead of Terminal because of the faster printing speed.  
+
+The default size requires you to use font size 5.
